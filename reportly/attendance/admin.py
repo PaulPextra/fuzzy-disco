392314b0ede4501feefc9d5169c3ae3cc7894be1
Paul Okoli <paulokoli7@gmail.com>
2021-10-28 21:18:30 +0100

    models update

--- reportly/attendance/admin.py
+++ reportly/attendance/admin.py
@@ -4,10 +4,15 @@ from .models import Book, Student
 # Register your models here.
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ['title','no_of_pages', 'isbn', 'date']
+    list_display = ['title','id', 'no_of_pages', 'isbn', 'date']
     list_editable = ['isbn']
     list_filter = ['date']
     list_per_page = 2
     search_fields = ['title', 'author', 'body']
 
-admin.site.register(Student)
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['name','id', 'gender', 'date_of_birth']
+    list_filter = ['gender']
+    list_per_page = 2
+    search_fields = ['name', 'gender', 'about']
--- reportly/attendance/models.py
+++ reportly/attendance/models.py
@@ -1,17 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Book(models.Model):
-    title = models.CharField(max_length=300)
-    no_of_pages = models.IntegerField(default=10)
-    author = models.CharField(max_length=300)
-    isbn = models.CharField(max_length=42, null=True, blank=True)
-    body = models.TextField()
-    date = models.DateTimeField()
-
-    def __str__(self):
-        return self.title
-
 class Student(models.Model):
     name = models.CharField(max_length=300)
     gender = models.CharField(max_length=300)
@@ -20,5 +9,18 @@ class Student(models.Model):
 
     def __str__(self):
         return self.name
+
+class Book(models.Model):
+    title = models.CharField(max_length=300)
+    no_of_pages = models.IntegerField(default=10)
+    student = models.ForeignKey(Student, on_delete=models.CASCADE, related_name='books')
+    author = models.CharField(max_length=300)
+    isbn = models.CharField(max_length=42, null=True, blank=True)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
 
     
